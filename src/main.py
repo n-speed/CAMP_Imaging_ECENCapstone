6184e1b80413c2e35da9ca4369cba661e034962c
Nathaniel Speed <speed.nathaniel@outlook.com>
2023-10-30 11:05:10 -0500

    rearranged some folder. rudimentary data importing works

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-import modeling as m
-import dataset.data_import as di
+import os
+
+#import modeling.VI_Model
+from tools.data_import import data_import
+
+#data_path = os.chdir("src/tools/raw_data/2023_Sample_Data")
+
+data_path = "tools/raw_data/2023_Sample_Data/cotton/EXG_2023.csv"
+
+data_import(data_path)
--- src/tools/data_import.py
+++ src/tools/data_import.py
@@ -1,12 +1,13 @@
-import numpy as np
+import os
 import pandas as pd
 
-def data_import(datapath, dataset):
+def data_import(datapath):
     """Simple function for importing data into a Pandas Dataframe
         datapath -- OS datapath to the .csv file
         dataset -- which dataset frame will be populated
     """
+    
     df = pd.read_csv(datapath)
-    return df
+    return print(df)
 
 
