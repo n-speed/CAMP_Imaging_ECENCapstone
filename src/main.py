fdef67adbfb6b2cc35774c07a028b3c28b3b61a4
Nathaniel Speed <speed.nathaniel@outlook.com>
2023-11-28 15:07:40 -0600

    [DEMO] All sets trainable now. Hacky implementation using while loop, but functional for now

--- src/main.py
+++ src/main.py
@@ -14,69 +14,76 @@ print('Runtime has {:.1f} gigabytes of available RAM \n'.format(ram_gb))
 
 input_size = 20
 num_layers = 5
-hidden_size = 10
+hidden_size = 1
 output_size = 1
 
 device = TSM.device
-model = TSM.LSTMModel(input_size,hidden_size, num_layers).to(device)
+model = TSM.LSTMModel(input_size, hidden_size, num_layers).to(device)
 loss_fn = torch.nn.MSELoss(reduction = 'mean')
 optimzier_obj = torch.optim.Adam(model.parameters(), lr = 1e-3)
 
 
 raw_data = importing()
 train, test = formatting(raw_data,0.75)
-x_train, y_train = train_sequence(train)
-x_test, y_test = test_sequence(test)
 
-batch_size = 16
-# Create DataLoader for batch training
-train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+i=0
+for i in range(4):
+	x_train, y_train = train_sequence(train[i])
+	x_test, y_test = test_sequence(test[i])
 
-# Create DataLoader for batch training
-test_dataset = torch.utils.data.TensorDataset(x_test, y_test)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+	batch_size = 8
+	# Create DataLoader for batch training
+	train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
+	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
 
-num_epochs = 100
-train_hist =[]
-test_hist =[]
-# Training loop
-for epoch in range(num_epochs):
-	total_loss = 0.0
+	# Create DataLoader for batch training
+	test_dataset = torch.utils.data.TensorDataset(x_test, y_test)
+	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-	# Training
-	model.train()
-	for batch_x, batch_y in train_loader:
-		batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-		predictions = model(batch_x)
-		loss = loss_fn(predictions, batch_y)
+	num_epochs = 50
+	train_hist =[]
+	test_hist =[]
+	# Training loop
+	for epoch in range(num_epochs):
+		total_loss = 0.0
 
-		optimzier_obj.zero_grad()
-		loss.backward()
-		optimzier_obj.step()
+		# Training
+		model.train()
+		for batch_x, batch_y in train_loader:
+			batch_x, batch_y = batch_x.to(device), batch_y.to(device)
+			predictions = model(batch_x)
+			loss = loss_fn(predictions, batch_y)
 
-		total_loss += loss.item()
+			optimzier_obj.zero_grad()
+			loss.backward()
+			optimzier_obj.step()
 
-	# Calculate average training loss and accuracy
-	average_loss = total_loss / len(train_loader)
-	train_hist.append(average_loss)
+			total_loss += loss.item()
 
-	# Validation on test data
-	model.eval()
-	with torch.no_grad():
-		total_test_loss = 0.0
+		# Calculate average training loss and accuracy
+		average_loss = total_loss / len(train_loader)
+		train_hist.append(average_loss)
 
-		for batch_X_test, batch_y_test in test_loader:
-			batch_X_test, batch_y_test = batch_X_test.to(device), batch_y_test.to(device)
-			predictions_test = model(batch_X_test)
-			test_loss = loss_fn(predictions_test, batch_y_test)
+		# Validation on test data
+		model.eval()
+		with torch.no_grad():
+			total_test_loss = 0.0
 
-			total_test_loss += test_loss.item()
+			for batch_X_test, batch_y_test in test_loader:
+				batch_X_test, batch_y_test = batch_X_test.to(device), batch_y_test.to(device)
+				predictions_test = model(batch_X_test)
+				test_loss = loss_fn(predictions_test, batch_y_test)
 
-		# Calculate average test loss and accuracy
-		average_test_loss = total_test_loss / len(test_loader)
-		test_hist.append(average_test_loss)
-	if (epoch+1)%5==0:
-		print(f'Epoch [{epoch+1}/{num_epochs}] - Training Loss: {average_loss:.4f}, Test Loss: {average_test_loss:.4f}')
+				total_test_loss += test_loss.item()
 
-graphing.graphLoss(num_epochs,train_hist,test_hist)
+			# Calculate average test loss and accuracy
+			average_test_loss = total_test_loss / len(test_loader)
+			test_hist.append(average_test_loss)
+		if (epoch+1)%5==0:
+			print(f'Epoch [{epoch+1}/{num_epochs}] - Training Loss: {average_loss:.4f}, Test Loss: {average_test_loss:.4f}')
+
+	graphing.graphLoss(num_epochs,train_hist,test_hist)
+
+	train_hist.clear() # excessive, but guarantees trash collection of the history arrays
+	test_hist.clear()
+	print()
--- src/modeling/sequence.py
+++ src/modeling/sequence.py
@@ -2,29 +2,27 @@ import numpy as np
 import torch
 
 def train_sequence(train_data):
-    temp_ary = train_data[0]
     seq_length = 1 # looking at previous week's projection
-    x_train, y_train = [] , [] # training just the cc data for now
-    for i in range(len(temp_ary)-seq_length):
-        x_train.append(temp_ary[i:i+seq_length])
-        y_train.append(temp_ary[i+1:i+seq_length+1])
+    x_train, y_train = [] , []
+    for i in range(len(train_data)-seq_length):
+        x_train.append(train_data[i:i+seq_length])
+        y_train.append(train_data[i+1:i+seq_length+1])
     x_train, y_train = np.array(x_train), np.array(y_train)
     
     x_train = torch.tensor(x_train, dtype = torch.float32)
     y_train = torch.tensor(y_train, dtype = torch.float32)
-    #print(x_train.shape,y_train.shape)
+    # print(x_train.shape,y_train.shape)
 
     return x_train, y_train
     # return print(x_train.shape,y_train.shape)
 
 
 def test_sequence(test_data):
-    temp_ary = test_data[0]
     seq_length = 1
-    x_test, y_test = [],[] # testing just the cc test data for now
-    for i in range(len(temp_ary)-seq_length):
-        x_test.append(temp_ary[i:i+seq_length])
-        y_test.append(temp_ary[i+1:i+seq_length+1])
+    x_test, y_test = [],[]
+    for i in range(len(test_data)-seq_length):
+        x_test.append(test_data[i:i+seq_length])
+        y_test.append(test_data[i+1:i+seq_length+1])
     x_test, y_test = np.array(x_test), np.array(y_test)
 
     x_test = torch.tensor(x_test, dtype = torch.float32)
